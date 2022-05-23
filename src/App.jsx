4598bc2ce84ef31bedfc42db22bfefeffe560eb3
Rino Takase <70308286+rino86wn@users.noreply.github.com>
2022-05-23 13:37:07 +0900

    React入門

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { ColorfullMessage } from "./components/ColorfullMessage";
+
+const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setfaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setfaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setfaceShowFlag(true);
+      } else {
+        faceShowFlag && setfaceShowFlag(false);
+      }
+    }
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
+      <ColorfullMessage color="pink">元気です!</ColorfullMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( ・∇・)</p>}
+    </>
+  );
+};
+
+export default App;
